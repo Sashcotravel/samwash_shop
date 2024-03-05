c976da199313cde6a81155ba9b4b0880a1206609
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2024-03-05 18:30:21 +0200

    update lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "@wemake-services/typescript/recommended",
+    "@wemake-services/javascript"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
